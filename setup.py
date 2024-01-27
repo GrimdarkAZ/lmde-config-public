f8bd29a723e3ba7ae279dcf2697917237bb49986
GrimdarkAZ <57446783+GrimdarkAZ@users.noreply.github.com>
2024-01-27 19:51:43 +0000

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,35 +1,50 @@
-import subprocess, sys
+import subprocess, sys, os
 import getpass
-
-sudo_password = getpass.getpass(prompt='sudo password: ')
 
 sys.path.append('/usr/lib/python3/dist-packages/')
 
+# Set initial variables
+
+sudo_password = getpass.getpass(prompt='sudo password: ')
+
+# Define functions
 def install_package(p):
     try:
         out, err = p.communicate(input=(sudo_password+'\n').encod(),timeout=5)
     except subprocess.TimeoutExpired:
         p.kill()
 
-try:
-    # Attempt to import the apt module
-    import apt
-except ImportError:
-    p = subprocess.Popen(['["sudo", "apt", "update"]'], stderr=subprocess.PIPE, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+# Create venv for ansible
 
-    install_package(p)
-    # subprocess.run(["sudo", "apt", "update"])
-    # subprocess.run(["sudo", "apt", "install", "-y", "python3-apt"])
-    import apt
+os.mkdir("~/.deployment", mode=0o750,*,dir_fd = None)
 
-cache = apt.cache.Cache()
-cache.update()
+subprocess.run(['python3','-m','venv','~/.deployment/.venv/'])
 
-# if cache['python3-venv'].is_installed:
-#     print('"python3-venv" already installed.')
-# else:
-#     subprocess.run(["sudo", "apt", "install", "-y", "python3-vevn", check=True])
-#     if cache['python3-venv'].is_installed:
-#         print('"python3-venv"' installed successfully.)
+# Activate venv for ansible
 
-# subprocess.run(["pip", "install", "ansible"])
+subprocess.run(['source','~/.deployment/.venv/bin/activate'])
+
+subprocess.run(['pip','install','ansible'])
+
+# try:
+#     # Attempt to import the apt module
+#     import apt
+# except ImportError:
+#     p = subprocess.Popen(['["sudo", "apt", "update"]'], stderr=subprocess.PIPE, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+
+#     install_package(p)
+#     # subprocess.run(["sudo", "apt", "update"])
+#     # subprocess.run(["sudo", "apt", "install", "-y", "python3-apt"])
+#     import apt
+
+# cache = apt.cache.Cache()
+# cache.update()
+
+# # if cache['python3-venv'].is_installed:
+# #     print('"python3-venv" already installed.')
+# # else:
+# #     subprocess.run(["sudo", "apt", "install", "-y", "python3-vevn", check=True])
+# #     if cache['python3-venv'].is_installed:
+# #         print('"python3-venv"' installed successfully.)
+
+# # subprocess.run(["pip", "install", "ansible"])
